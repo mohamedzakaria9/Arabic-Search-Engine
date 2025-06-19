04bb1057ab2e1f7d19ce7b2a11abe4fb22ccedfe
mohamedzakaria9 <mohamed.zakaria.3@hotmail.com>
2025-06-20 02:11:07 +0300

    added the book description page

--- lib/home.dart
+++ lib/home.dart
@@ -1,24 +1,12 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'Book.dart';
+import 'BookDetailsPage.dart';
 import 'BookSearchScreen.dart';
 
 // Book model
-class Book {
-  final String title;
-  final String author;
-  final String image;
 
-  Book({required this.title, required this.author, required this.image});
-
-  factory Book.fromJson(Map<String, dynamic> json) {
-    return Book(
-      title: json['title'] ?? '',
-      author: json['author'] ?? '',
-      image: json['image'] ?? '',
-    );
-  }
-}
 
 class home extends StatefulWidget {
   const home({Key? key}) : super(key: key);
@@ -42,7 +30,9 @@ class _BooksPageState extends State<home> {
     loadBooks();
 
     _scrollController.addListener(() {
-      if (_scrollController.position.pixels >= _scrollController.position.maxScrollExtent - 300 && !isFetchingMore) {
+      if (_scrollController.position.pixels >=
+              _scrollController.position.maxScrollExtent - 300 &&
+          !isFetchingMore) {
         loadMoreBooks();
       }
     });
@@ -84,7 +74,9 @@ class _BooksPageState extends State<home> {
   }
 
   Future<Map<String, List<Book>>> fetchBooks({int page = 1}) async {
-    final url = Uri.parse('https://search-engine-server-4qq9.vercel.app?page=$page');
+    final url = Uri.parse(
+      'https://search-engine-server-4qq9.vercel.app?page=$page',
+    );
     final response = await http.get(url);
 
     print('📦 Raw response: ${response.body}');
@@ -92,15 +84,19 @@ class _BooksPageState extends State<home> {
     if (response.statusCode == 200) {
       final List<dynamic> jsonData = json.decode(response.body);
 
-      List<Book> allBooks = jsonData.map((item) => Book(
-        title: item['book_title'] ?? 'No title',
-        author: item['book_author'] ?? 'Unknown author',
-        image: item['book_cover_photo_url'] ?? '',
-      )).toList();
+      List<Book> allBooks =
+          jsonData
+              .map(
+                (item) => Book(
+                  title: item['book_title'] ?? 'No title',
+                  author: item['book_author'] ?? 'Unknown author',
+                  image: item['book_cover_photo_url'] ?? '',
+                  description: item['book_description'] ?? 'No description',
+                ),
+              )
+              .toList();
 
-      return {
-        "الكل": allBooks,
-      };
+      return {"الكل": allBooks};
     } else {
       throw Exception('Failed to load books');
     }
@@ -116,71 +112,73 @@ class _BooksPageState extends State<home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("المكتبة")),
-      body: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ListView(
-        controller: _scrollController,
-        children: [
-          Stack(
-            children: [
-              Image.asset(
-                "images/Book in library image.png",
-                width: double.infinity,
-                height: 271,
-                fit: BoxFit.cover,
-              ),
-              Positioned(
-                left: 0,
-                right: 0,
-                top: 100,
-                child: Text(
-                  'ابحث في عالم مليء بالكتب',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontFamily: 'Arabic Typesetting',
-                    fontWeight: FontWeight.w400,
-                    shadows: [
-                      Shadow(
-                        offset: Offset(0, 4),
-                        blurRadius: 4,
-                        color: Colors.black.withOpacity(0.84),
+      body:
+          isLoading
+              ? Center(child: CircularProgressIndicator())
+              : ListView(
+                controller: _scrollController,
+                children: [
+                  Stack(
+                    children: [
+                      Image.asset(
+                        "images/Book in library image.png",
+                        width: double.infinity,
+                        height: 271,
+                        fit: BoxFit.cover,
+                      ),
+                      Positioned(
+                        left: 0,
+                        right: 0,
+                        top: 100,
+                        child: Text(
+                          'ابحث في عالم مليء بالكتب',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontFamily: 'Arabic Typesetting',
+                            fontWeight: FontWeight.w400,
+                            shadows: [
+                              Shadow(
+                                offset: Offset(0, 4),
+                                blurRadius: 4,
+                                color: Colors.black.withOpacity(0.84),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
                     ],
                   ),
-                ),
+                  const SizedBox(height: 20),
+                  _buildSearchBox(),
+                  const SizedBox(height: 20),
+                  _buildCategorySelector(),
+                  const SizedBox(height: 10),
+                  GridView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10,
+                          childAspectRatio: 0.6,
+                        ),
+                    itemCount: books[selectedCategory]?.length ?? 0,
+                    itemBuilder: (context, index) {
+                      return _buildBookItem(books[selectedCategory]![index]);
+                    },
+                  ),
+                  if (isFetchingMore)
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
+                ],
               ),
-            ],
-          ),
-          const SizedBox(height: 20),
-          _buildSearchBox(),
-          const SizedBox(height: 20),
-          _buildCategorySelector(),
-          const SizedBox(height: 10),
-          GridView.builder(
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              childAspectRatio: 0.6,
-            ),
-            itemCount: books[selectedCategory]?.length ?? 0,
-            itemBuilder: (context, index) {
-              return _buildBookItem(books[selectedCategory]![index]);
-            },
-          ),
-          if (isFetchingMore)
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: CircularProgressIndicator(),
-              ),
-            ),
-        ],
-      ),
     );
   }
 
@@ -198,10 +196,7 @@ class _BooksPageState extends State<home> {
         decoration: ShapeDecoration(
           color: const Color(0xFFF9FAFA),
           shape: RoundedRectangleBorder(
-            side: BorderSide(
-              width: 1,
-              color: const Color(0xFF6E152E),
-            ),
+            side: BorderSide(width: 1, color: const Color(0xFF6E152E)),
             borderRadius: BorderRadius.circular(15),
           ),
         ),
@@ -222,9 +217,10 @@ class _BooksPageState extends State<home> {
       height: 50,
       child: ListView(
         scrollDirection: Axis.horizontal,
-        children: books.keys.map((category) {
-          return _buildCategoryButton(category);
-        }).toList(),
+        children:
+            books.keys.map((category) {
+              return _buildCategoryButton(category);
+            }).toList(),
       ),
     );
   }
@@ -255,49 +251,56 @@ class _BooksPageState extends State<home> {
   }
 
   Widget _buildBookItem(Book book) {
-    return SizedBox(
-      width: 100,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.network(
-            book.image,
-            width: 100,
-            height: 150,
-            fit: BoxFit.cover,
-            errorBuilder: (context, error, stackTrace) =>
-            const Icon(Icons.broken_image, size: 100),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => BookDetailsPage(book: book),
           ),
-          const SizedBox(height: 5),
-          Directionality(
-            textDirection: TextDirection.rtl,
-            child: Text(
-              book.title,
-              textAlign: TextAlign.left,
-              style: const TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.bold,
+        );
+      },
+      child: SizedBox(
+        width: 100,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                book.image,
+                width: 100,
+                height: 150,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) =>
+                const Icon(Icons.broken_image, size: 100),
               ),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              softWrap: false,
             ),
-          ),
-          Directionality(
-            textDirection: TextDirection.rtl,
-            child: Text(
-              book.author,
-              textAlign: TextAlign.left,
-              style: const TextStyle(
-                fontSize: 12,
-                color: Colors.grey,
+            const SizedBox(height: 5),
+            Directionality(
+              textDirection: TextDirection.rtl,
+              child: Text(
+                book.title,
+                textAlign: TextAlign.left,
+                style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                softWrap: false,
               ),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              softWrap: false,
             ),
-          ),
-        ],
+            Directionality(
+              textDirection: TextDirection.rtl,
+              child: Text(
+                book.author,
+                textAlign: TextAlign.left,
+                style: const TextStyle(fontSize: 12, color: Colors.grey),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                softWrap: false,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
